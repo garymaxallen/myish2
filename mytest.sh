154119fae6d79f7452f9a117f89fb24c3b43026a
pcl <598369987@qq.com>
2022-10-19 16:26:24 +0800

    xxx

---
+++ mytest.sh
@@ -0,0 +1,25 @@
+starttime=$(date +%s)
+myhome="/Users/pcl/Documents/tmp/myish2"
+cd $myhome
+xcrun simctl uninstall booted app.ish.iSH
+rm -rf build mesonbuild
+meson setup mesonbuild
+cd mesonbuild
+ninja -v
+cd ..
+mkdir -p build/Release-iphonesimulator
+cd build/Release-iphonesimulator
+rm -rf meson
+ln -s $myhome/mesonbuild meson
+cd ../..
+xcodebuild build CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO -sdk iphonesimulator15.5 -target libfakefs
+xcodebuild build CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO -sdk iphonesimulator15.5 -target libish_emu
+xcodebuild build CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO -sdk iphonesimulator15.5 -target liblinux
+xcodebuild build CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO -arch x86_64 -sdk iphonesimulator15.5 -target iSHFileProvider
+xcodebuild build CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO -arch x86_64 -sdk iphonesimulator15.5 -target iSH
+cp ish_resources/hterm_all.js build/Release-iphonesimulator/iSH.app/
+cp ish_resources/root.tar.gz build/Release-iphonesimulator/iSH.app/
+xcrun simctl install "iPhone 12 Pro Max" build/Release-iphonesimulator/iSH.app
+xcrun simctl launch booted app.ish.iSH
+endtime=$(date +%s)
+echo run time: $(expr $endtime - $starttime)s
