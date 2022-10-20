31d0c632530742d69e0a773dc7d18b7ae3c8aeeb
pcl <598369987@qq.com>
2022-10-20 11:50:18 +0800

    xxx

---
+++ mytest-reduced.sh
@@ -0,0 +1,13 @@
+starttime=$(date +%s)
+xcrun simctl uninstall booted app.ish.iSH
+rm -rf build
+mkdir -p build/Release-iphonesimulator
+cp ish_resources/lib* build/Release-iphonesimulator/
+cp ish_resources/infoplisticons.h build/Release-iphonesimulator/
+xcodebuild build CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO -arch x86_64 -sdk iphonesimulator15.5 -target iSH
+cp ish_resources/hterm_all.js build/Release-iphonesimulator/iSH.app/
+cp ish_resources/root.tar.gz build/Release-iphonesimulator/iSH.app/
+xcrun simctl install "iPhone 12 Pro Max" build/Release-iphonesimulator/iSH.app
+xcrun simctl launch booted app.ish.iSH
+endtime=$(date +%s)
+echo run time: $(expr $endtime - $starttime)s
