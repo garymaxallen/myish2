af60db8d3f60cb3ac16f6bbe7eb457749cdb7045
pcl <598369987@qq.com>
2022-10-21 22:02:32 +0800

    xxx

--- mytest-reduced.sh
+++ mytest-reduced.sh
@@ -5,6 +5,7 @@ mkdir -p build/Release-iphonesimulator
 cp ish_resources/lib* build/Release-iphonesimulator/
 cp ish_resources/infoplisticons.h build/Release-iphonesimulator/
 xcodebuild build CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO -arch x86_64 -sdk iphonesimulator15.5 -target iSH
+cp ish_resources/repositories.txt build/Release-iphonesimulator/iSH.app/
 cp ish_resources/hterm_all.js build/Release-iphonesimulator/iSH.app/
 cp ish_resources/root.tar.gz build/Release-iphonesimulator/iSH.app/
 xcrun simctl install "iPhone 12 Pro Max" build/Release-iphonesimulator/iSH.app
