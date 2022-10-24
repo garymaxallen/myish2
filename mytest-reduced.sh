f743b72aa0db2e6ac4288797639ccd4e0009f3cd
pcl <598369987@qq.com>
2022-10-24 15:44:35 +0800

    xxx

--- mytest-reduced.sh
+++ mytest-reduced.sh
@@ -8,6 +8,7 @@ xcodebuild build CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO -arch x86_64 -sd
 cp ish_resources/repositories.txt build/Release-iphonesimulator/iSH.app/
 cp ish_resources/hterm_all.js build/Release-iphonesimulator/iSH.app/
 cp ish_resources/root.tar.gz build/Release-iphonesimulator/iSH.app/
+cp ish_resources/term.* build/Release-iphonesimulator/iSH.app/
 xcrun simctl install "iPhone 12 Pro Max" build/Release-iphonesimulator/iSH.app
 xcrun simctl launch booted app.ish.iSH
 endtime=$(date +%s)
