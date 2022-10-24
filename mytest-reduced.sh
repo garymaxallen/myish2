780472eb721d81c63b489fe9c1bf868b32b649c6
pcl <598369987@qq.com>
2022-10-24 18:10:27 +0800

    xxx

--- mytest-reduced.sh
+++ mytest-reduced.sh
@@ -3,7 +3,7 @@ xcrun simctl uninstall booted app.ish.iSH
 rm -rf build
 mkdir -p build/Release-iphonesimulator
 cp ish_resources/lib* build/Release-iphonesimulator/
-cp ish_resources/infoplisticons.h build/Release-iphonesimulator/
+#cp ish_resources/infoplisticons.h build/Release-iphonesimulator/
 xcodebuild build CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO -arch x86_64 -sdk iphonesimulator15.5 -target iSH
 cp ish_resources/repositories.txt build/Release-iphonesimulator/iSH.app/
 cp ish_resources/hterm_all.js build/Release-iphonesimulator/iSH.app/
