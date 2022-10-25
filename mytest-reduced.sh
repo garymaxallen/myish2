2407b4a4d1b92f11ec84cb3bace10d71112f2af7
pcl <598369987@qq.com>
2022-10-25 10:23:01 +0800

    xxx

--- mytest-reduced.sh
+++ mytest-reduced.sh
@@ -2,13 +2,13 @@ starttime=$(date +%s)
 xcrun simctl uninstall booted app.ish.iSH
 rm -rf build
 mkdir -p build/Release-iphonesimulator
-cp ish_resources/lib* build/Release-iphonesimulator/
-#cp ish_resources/infoplisticons.h build/Release-iphonesimulator/
+cp myresource/lib* build/Release-iphonesimulator/
+#cp myresource/infoplisticons.h build/Release-iphonesimulator/
 xcodebuild build CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO -arch x86_64 -sdk iphonesimulator15.5 -target iSH
-cp ish_resources/repositories.txt build/Release-iphonesimulator/iSH.app/
-cp ish_resources/hterm_all.js build/Release-iphonesimulator/iSH.app/
-cp ish_resources/root.tar.gz build/Release-iphonesimulator/iSH.app/
-cp ish_resources/term.* build/Release-iphonesimulator/iSH.app/
+cp myresource/repositories.txt build/Release-iphonesimulator/iSH.app/
+cp myresource/hterm_all.js build/Release-iphonesimulator/iSH.app/
+cp myresource/root.tar.gz build/Release-iphonesimulator/iSH.app/
+cp myresource/term.* build/Release-iphonesimulator/iSH.app/
 xcrun simctl install "iPhone 12 Pro Max" build/Release-iphonesimulator/iSH.app
 xcrun simctl launch booted app.ish.iSH
 endtime=$(date +%s)
